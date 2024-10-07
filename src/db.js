41202d07902c6d262349b477e902166f85a8a14f
Nimith <sallurinimith.sn@gmail.com>
2024-10-07 09:25:56 +0530

    hi

---
+++ src/db.js
@@ -0,0 +1,38 @@
+const data=[
+    { 
+        id:1,
+        img:"https://i.pinimg.com/236x/4d/7a/be/4d7abe7eecce95ab9bb10d13df22e9f2.jpg",
+        name:"Roman Reigns",
+        age:"40"
+    },
+    {
+        id:2,
+        img:"https://i.pinimg.com/236x/34/f5/4d/34f54d22211cee9cfd13e314e530a806.jpg",
+        name:"Matthew Parker",
+        age:"30"
+    },
+    {
+        id:3,
+        img:"https://i.pinimg.com/236x/94/ed/33/94ed33ced8f4bd456f559dc0c1e143a8.jpg",
+        name:"Emily Anderson",
+        age:"38"
+    },
+    {
+        id:4,
+        img:"https://i.pinimg.com/236x/94/ed/33/94ed33ced8f4bd456f559dc0c1e143a8.jpg",
+        name:"Sophia Roberts",
+        age:"31"
+    },
+    {
+        id:5,
+        img:"https://i.pinimg.com/236x/4a/06/d8/4a06d8086dcbc59efe1465b62307fd5b.jpg",
+        name:"Emma Mitchell",
+        age:"45"
+    },{
+        id:6,
+        img:"https://i.pinimg.com/236x/1b/eb/bf/1bebbfa51353c1efbfbbb1bec09d79f7.jpg",
+        name:"Charlotte Davis",
+        age:"35"
+    }
+]
+export default data
