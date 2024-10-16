527dbb6770d42c26080b0c20e4ac497d7491bae2
Nimith <sallurinimith.sn@gmail.com>
2024-10-16 15:17:17 +0530

    commit

--- src/db.js
+++ src/db.js
@@ -1,38 +1,39 @@
-const data=[
-    { 
-        id:1,
-        img:"https://i.pinimg.com/236x/4d/7a/be/4d7abe7eecce95ab9bb10d13df22e9f2.jpg",
-        name:"Roman Reigns",
-        age:"40"
-    },
-    {
-        id:2,
-        img:"https://i.pinimg.com/236x/34/f5/4d/34f54d22211cee9cfd13e314e530a806.jpg",
-        name:"Matthew Parker",
-        age:"30"
-    },
-    {
-        id:3,
-        img:"https://i.pinimg.com/236x/94/ed/33/94ed33ced8f4bd456f559dc0c1e143a8.jpg",
-        name:"Emily Anderson",
-        age:"38"
-    },
-    {
-        id:4,
-        img:"https://i.pinimg.com/236x/94/ed/33/94ed33ced8f4bd456f559dc0c1e143a8.jpg",
-        name:"Sophia Roberts",
-        age:"31"
-    },
-    {
-        id:5,
-        img:"https://i.pinimg.com/236x/4a/06/d8/4a06d8086dcbc59efe1465b62307fd5b.jpg",
-        name:"Emma Mitchell",
-        age:"45"
-    },{
-        id:6,
-        img:"https://i.pinimg.com/236x/1b/eb/bf/1bebbfa51353c1efbfbbb1bec09d79f7.jpg",
-        name:"Charlotte Davis",
-        age:"35"
-    }
-]
-export default data
+let db = [
+        {
+            id:"1",
+            name:"David Mitchell",
+            date:"22-02-2002",
+            image:"https://cdn.pixabay.com/photo/2018/10/29/21/46/human-3782189_640.jpg"
+        },
+        {
+            id:"2",
+            name:"Alexander Carter",
+            date:"07-12-2000",
+            image:"https://cdn.pixabay.com/photo/2023/04/21/15/42/portrait-7942151_640.jpg"
+        },
+        {
+            id:"3",
+            name:"Olivia Johnson",
+            date:"04-10-1995",
+            image:"https://cdn.pixabay.com/photo/2021/04/10/05/32/woman-6166085_640.jpg"
+        },
+        {
+            id:"4",
+            name:"Ryan Collins",
+            date:"17-05-1995",
+            image:"https://cdn.pixabay.com/photo/2015/08/05/04/02/people-875597_640.jpg"
+        },
+        {
+            id:"5",
+            name:"Emily Walker",
+            date:"14-11-1988",
+            image:"https://cdn.pixabay.com/photo/2018/01/02/09/47/woman-3055841_640.jpg"
+        },{
+            id:"6",
+            name:"Grace Parker",
+            date:"26-01-1985",
+            image:"https://cdn.pixabay.com/photo/2017/06/06/16/48/woman-2377742_640.jpg"
+        }
+    ]
+
+export default db;
